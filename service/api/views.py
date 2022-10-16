35da99d3de1b921a40e4fbca0541a5f3217fe4e1
WaveOfCoding <waveformwstcrz@gmail.com>
2022-10-16 17:09:10 +0300

    feature: Rename watchlist

--- service/api/views.py
+++ service/api/views.py
@@ -12,7 +12,7 @@ class MovieDetail(APIView):
             rating = request.query_params.get('rating', None)
             if rating is not None:
                 filters['rating'] = rating
-            watchlist = request.query_params.get('watch_list', None)
+            watchlist = request.query_params.get('watchlist', None)
             if watchlist is not None:
                 if watchlist == 'true':
                     filters['watchlist'] = True
@@ -68,37 +68,3 @@ class MovieInfo(APIView):
 
         movies.delete()
         return Response({'msg': 'deleted'}, status=status.HTTP_204_NO_CONTENT)
-
-
-
-
-# class MovieDetail(APIView):
-#     def get(self, request):
-#         queryset = WatchList.objects.all()
-#         serializer = WatchSerializer(queryset, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-# @api_view(['GET', 'POST'])
-# def get_movies(request, format=None):
-#     if request.method == 'GET':
-#         filters = {}
-#         rating = request.query_params.get('rating', None)
-#         if rating is not None:
-#             filters['rating'] = rating
-#         watch_list = request.query_params.get('watch_list', None)
-#         if watch_list is not None:
-#             if watch_list == 'true':
-#                 filters['watch_list'] = True
-#             else:
-#                 filters['watch_list'] = False
-#         movies = WatchList.objects.all().filter(**filters)
-#
-#         serializer = WatchSerializer(movies, many=True)
-#         return Response(serializer.data)
-#     if request.method == 'POST':
-#         serializer = WatchSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
