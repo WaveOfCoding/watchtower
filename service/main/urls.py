c43f4e0c0024b3759a7a9f20c7e90fd4f71eb21e
WaveOfCoding <waveformwstcrz@gmail.com>
2022-10-16 16:07:50 +0300

    feature: Add change/delete movie endpoints

--- service/main/urls.py
+++ service/main/urls.py
@@ -14,16 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+# from rest_framework.urlpatterns import format_suffix_patterns
 
 from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', views.get_movies),
+    path('api/', include('api.urls')),
+    # path('movies/', views.get_movies),
     # path('movies/<int:id>', views.watch_detail),
 
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
