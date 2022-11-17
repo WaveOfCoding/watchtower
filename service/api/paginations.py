7c694c8716a08c8dc28799f8f433963bdd34c64b
WaveOfCoding <waveformwstcrz@gmail.com>
2022-11-17 21:35:27 +0300

    bugfix: Fix paginations fields

--- service/api/paginations.py
+++ service/api/paginations.py
@@ -4,12 +4,12 @@ from rest_framework.response import Response
 
 
 class MovieAPIListPagination(pagination.PageNumberPagination):
-    page_size = 2
+    page_size = 10
 
     def get_paginated_response(self, data):
         return Response(OrderedDict([
             ('total_results', self.page.paginator.count),
-            ('total_pages', self.page_size),
+            ('total_pages', self.page.paginator.num_pages),
             ('page', self.page.number),
             ('results', data)
         ]))
